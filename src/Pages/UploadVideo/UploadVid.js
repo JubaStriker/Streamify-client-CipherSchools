2e723012b26e7caf0f5ea04137e37aa1887f5fe3
JubaStriker <jubastriker@gmail.com>
2023-03-11 17:46:57 +0600

    input section added

--- src/Pages/UploadVideo/UploadVid.js
+++ src/Pages/UploadVideo/UploadVid.js
@@ -1,17 +1,22 @@
 import { Button, Input, Textarea } from '@material-tailwind/react';
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import Dropzone from 'react-dropzone';
+import { toast } from 'react-hot-toast';
 import { ImUpload } from 'react-icons/im'
 
 const UploadVid = () => {
 
+    const [file, setFile] = useState("")
     const onDrop = (files) => {
         let formData = new FormData();
         const config = {
             header: { 'content-type': 'multipart/form-data' }
         }
-
+        console.log(files[0].name)
         formData.append("file", files[0])
+        setFile(files[0].name)
+
     }
     return (
         <div className='md:mx-10 md:px-10 mt-10 mx-2'>
@@ -24,15 +29,19 @@ const UploadVid = () => {
                         {({ getRootProps, getInputProps }) => (
                             <section>
                                 <div className='w-[300px] h-[240px] border-2 border-gray-300 border-dashed hover:border-blue-500' {...getRootProps()}>
-                                    <input {...getInputProps()} />
-                                    <div className='flex justify-center h-full text-gray-600 items-center text-4xl'><ImUpload /></div>
+                                    <input {...getInputProps()} accept=".mp4, .mkv" />
+                                    <div className='flex flex-col justify-center h-full text-gray-600 items-center text-4xl'>
+                                        <ImUpload />
+                                        {file ? <p className='text-sm'>{file}</p> : <p className='text-sm'>Drag and drop files here</p>}
+                                    </div>
+
                                 </div>
                             </section>
                         )}
                     </Dropzone>
 
                     <div className='mt-3 max-w-3xl'>
-                        <Input name='email' size="lg" label="Title" />
+                        <Input name='email' size="lg" label="Title" required />
                     </div>
                     <div className='mt-3 max-w-3xl'>
                         <Textarea variant="outlined" label="description" />
